b318301c80d342e46e2920f1057ef5441fd684c3
RomanKhrapai <hrapayr@gmail.com>
2022-02-14 12:44:26 +0200

    fix

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,7 +4,7 @@ import {AppContainer} from './AppContainer.styles'
 import Gallery from "./Gallery";
 import Searchbar from "./Searchbar";
 
-export default function App (){
+export function App (){
   const [searchText,setsearchText]= useState('')
 
  return (
